4c49da053037f16e81bdfbc694394d4a4680f278
Firmin <zephimir@gmail.com>
2022-11-01 09:56:34 +0100

    feat: dynamically set dummy seo data on page

--- app/head.tsx
+++ app/head.tsx
@@ -1,13 +1,26 @@
+import { use } from "react";
+
+type RootObject = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const fetchSomePosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data: RootObject[] = await response.json();
+  return data;
+};
+
 const Head = () => {
+  const [firstPost] = use(fetchSomePosts());
   return (
     <>
-      <title>Neurchis</title>
+      <title>{"zqd" + firstPost.title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width" />
-      <meta
-        name="description"
-        content="Une application open source pour les communautés de Neurchis"
-      />
+      <meta name="description" content={firstPost.body} />
     </>
   );
 };
